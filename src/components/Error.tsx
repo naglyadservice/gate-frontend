b900b53ea918f8a90537ae7291e5e546e1412dbb
MrInverted <akson.dyuk@gmail.com>
2025-02-18 19:41:33 +0200

    minor css

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,6 +1,6 @@
 function Error() {
   return (
-    <div className='flex flex-col justify-center items-center h-full w-full'>
+    <div className='flex flex-col justify-center items-center min-h-[100vh] w-full'>
       <div className='text-xl sm:text-2xl font-semibold'>Some error</div>
     </div>
   )
--- src/components/LogInWithGoole.tsx
+++ src/components/LogInWithGoole.tsx
@@ -10,7 +10,7 @@ function LogInWithGoole() {
 
   return (
     <Layout>
-      <div className="flex flex-col justify-center items-center h-full w-full gap-3">
+      <div className="flex flex-col justify-center items-center min-h-[100vh] w-full gap-3">
         <p>Ви не авторизовані</p>
         <button
           className="inline-flex items-center justify-center gap-3 px-8 py-2 rounded-md border border-black hover:opacity-70"
